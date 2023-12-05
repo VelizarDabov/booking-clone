d9e0da395d3348d2fed93b86dc48116480505c9a
VelizarDabov <velizardabov@gmail.com>
2023-12-05 23:33:41 +0200

    nz

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import {trandingData} from '../data/Trending'
+import SearchForm from '@/components/SearchForm'
 export default function Home() {
   return (
     <main className="bg-[#013B94]">
@@ -7,7 +8,9 @@ export default function Home() {
     <h2 className='font-bold text-5xl text-white'> Find your Next Stay</h2>
     <h3 className='text-white py-5 text-xl'>Search low prices on hotels, homes and much more</h3>
   </section>
-  <section className='m-4 mt-0 -mb-14 px-2 lg:px-4 '></section>
+  <section className='m-4 mt-0 -mb-14 px-2 lg:px-4 '>
+    <SearchForm />
+  </section>
 
   <section className='mx-auto max-w-7xl mt-10 p-6 bg-white rounded-t-lg'>
 <div className='pt-5'>
